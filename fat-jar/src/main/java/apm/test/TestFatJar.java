54ebcb7fb4a103175f83896aedb1b8945041e980
Alvaro <Alvaro@APMDELL>
2019-01-08 21:18:49 +0100

    initial version

---
+++ fat-jar/src/main/java/apm/test/TestFatJar.java
@@ -0,0 +1,11 @@
+package apm.test;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class TestFatJar {
+
+	public static void main(String[] args) {
+		System.out.println(NumberUtils.max(1,2,3));
+	}
+
+}
